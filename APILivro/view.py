e28abef21e1633301fe96facc06c999276955848
frithunanths476 <124930953+frithunanths476@users.noreply.github.com>
2024-04-29 13:37:43 -0300

    Add files via upload

--- APILivro/view.py
+++ APILivro/view.py
@@ -4,26 +4,28 @@ from models import Livro, Usuario
 
 @app.route('/livro', methods=['GET'])
 def get_livro():
-    livros = Livro.query.all()
-    livros_dic = []
-    for livro in livros:
-        livro_dic = {
-            'id_livro' : livro.id_livro,
-             'titulo' : livro.titulo,
-             'autor' : livro.autor,
-             'ano_publicacao' : livro.ano_publicacao
-        }
-        livros_dic.append(livro_dic)
+    if 'id_usuario' in session:
+        livros = Livro.query.all()
+        livros_dic = []
+        for livro in livros:
+            livro_dic = {
+                'id_livro' : livro.id_livro,
+                 'titulo' : livro.titulo,
+                 'autor' : livro.autor,
+                 'ano_publicacao' : livro.ano_publicacao
+            }
+            livros_dic.append(livro_dic)
 
-    #retorna dicionario em formato json
-    return jsonify(
-            mensagem= 'Lista de Livros',
-            livros= livros_dic
-        )
+        #retorna dicionario em formato json
+        return jsonify(
+                mensagem= 'Lista de Livros',
+                livros= livros_dic
+            )
+    else:
+        return jsonify({'mensagem': 'Requer Autorização'})
 
 @app.route('/livro',  methods=['POST'])
 def post_livro():
-
     if 'id_usuario' in session:
         #pega dados do livro enviado pelo json
         livro = request.json
